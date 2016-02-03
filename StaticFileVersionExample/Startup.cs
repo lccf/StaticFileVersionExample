73243cc08bb1be67a0cf0d78821e4c1053fa6211
chenglifu <chenglifu@huazhu.com>
2016-02-03 14:20:47 +0800

    初始化项目

---
+++ StaticFileVersionExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StaticFileVersionExample.Startup))]
+namespace StaticFileVersionExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
