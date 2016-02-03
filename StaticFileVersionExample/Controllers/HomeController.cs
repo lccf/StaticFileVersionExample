57d4fc8b97d54ff2161f5ba032acfde6e122c23e
chenglifu <chenglifu@huazhu.com>
2016-02-03 16:53:40 +0800

    查询参数形式版本号演示

--- StaticFileVersionExample/Controllers/HomeController.cs
+++ StaticFileVersionExample/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace StaticFileVersionExample.Controllers
             return View();
         }
 
+        public ActionResult QueryString()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
